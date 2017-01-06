b68d5c521bfa03564b2faa32337549c1c36b3241
Burak Basbug <burakbasbug@ip149-148-10-11.x8021.meduniwien.ac.at>
2017-01-06 20:22:35 +0100

    registerUser(), lookup() and getNameserver() methods are implemented

--- dslab16/src/main/java/nameserver/NameserverService.java
+++ dslab16/src/main/java/nameserver/NameserverService.java
@@ -18,35 +18,55 @@ public class NameserverService implements INameserver, Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	private SortedMap<String, INameserver> subNameservers;
+	private SortedMap<String, String> userAddressMap;
 	
-	public NameserverService(SortedMap<String, INameserver> subNameservers) {
+	public NameserverService(SortedMap<String, INameserver> subNameservers, SortedMap<String, String> userAddressMap) {
 		this.subNameservers = subNameservers;
+		this.userAddressMap = userAddressMap;
 	}
 
 	@Override
 	public void registerUser(String username, String address)
 			throws RemoteException, AlreadyRegisteredException, InvalidDomainException {
-		System.out.println("register user invoked!!!!");
-
+		synchronized (userAddressMap) {
+			if(userAddressMap.containsKey(username)){
+				throw new AlreadyRegisteredException("User is already registered!");
+			}
+		}
+		int lastIndex = username.lastIndexOf(".");
+		if(lastIndex==-1){ //no "." in username
+			System.out.println("Registering new user: \'" + username + "\'.");
+			synchronized (userAddressMap) {
+				userAddressMap.put(username, address);
+			}
+		}else{
+			String parentZone = username.substring(lastIndex+1);
+			username = username.substring(0,lastIndex);
+			INameserverForChatserver subNs = null;
+			synchronized (subNameservers){						
+				subNs = subNameservers.get(parentZone);
+			}
+			if(subNs==null){
+				throw new InvalidDomainException("An intermediary zone does not exist!");
+			}
+			subNs.registerUser(username, address);
+		}
 	}
 
 	@Override
 	public INameserverForChatserver getNameserver(String zone) throws RemoteException {
-		System.out.println("get nameserver invoked!!!");
-		return null;
+		return (INameserverForChatserver) subNameservers.get(zone);
 	}
 
 	@Override
 	public String lookup(String username) throws RemoteException {
-		System.out.println("lookup invokation!!!");
-		return null;
+		return userAddressMap.get(username);
 	}
 
 	@Override
 	public void registerNameserver(String domain, INameserver nameserver,
 			INameserverForChatserver nameserverForChatserver)
-			throws RemoteException, AlreadyRegisteredException, InvalidDomainException {
-				
+			throws RemoteException, AlreadyRegisteredException, InvalidDomainException {		
 		synchronized (subNameservers) {
 			if(subNameservers.keySet().contains(domain)){
 				throw new AlreadyRegisteredException("This domain is already registered!");
@@ -56,23 +76,15 @@ public class NameserverService implements INameserver, Serializable{
 					System.out.println("Registering nameserver for zone \'" + domain + "\'");
 					subNameservers.put(domain,nameserver);
 				}else{
-					String beforeDot = domain.substring(0,lastIndex);
-					String afterDot = domain.substring(lastIndex+1);
-					
-					INameserver subNs = subNameservers.get(afterDot);
+					String childZone = domain.substring(0,lastIndex);
+					String parentZone = domain.substring(lastIndex+1);
+					INameserver subNs = subNameservers.get(parentZone);
 					if(subNs==null){
 						throw new InvalidDomainException("An intermediary zone does not exist!");
 					}
-					subNs.registerNameserver(beforeDot, nameserver, nameserverForChatserver);
+					subNs.registerNameserver(childZone, nameserver, nameserverForChatserver);
 				}
 			}
 		}		
 	}
-	
-	public SortedMap<String, INameserver> getSubNameservers(){
-		return this.subNameservers;
-	}
-
-	
-
 }
